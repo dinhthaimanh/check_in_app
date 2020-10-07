73fcc7976ab5e729833f91e967ca7b76b384056d
Manh Dinh <manh.dinh@fastboy.net>
2020-10-07 17:20:28 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.indigo,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Check in app'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _isCheckin = false;
+  bool _loading = false;
+  // var icon = _isCheckin? Icon(Icons.login): Icon(Icons.logout);
+  // var label = Text(_isCheckin ? 'Check in' : 'Check out');
+
+  Future<http.Response> fetchAlbum() async {
+    var res = await http.get('https://jsonplaceholder.typicode.com/albums/1');
+    print(res.body);
+    return res;
+  }
+
+  void _checkInOut() async {
+    var res = await fetchAlbum();
+    setState(() {
+      _loading = true;
+    });
+    if (res.statusCode == 200) {
+      setState(() {
+        _isCheckin = !_isCheckin;
+        _loading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: _checkInOut,
+              tooltip: _isCheckin ? 'Check in' : 'Check out',
+              // icon: _isCheckin
+              //     ? Icon(Icons.login)
+              //     : Icon(Icons.logout),
+              // label: Text(_isCheckin ? 'Check in' : 'Check out'),
+
+              backgroundColor:
+                  _isCheckin ? HexColor('#1890ff') : HexColor('#ff4d4f'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+}
